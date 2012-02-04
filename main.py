1d3eae3d87e41f04b79a81c5a51f1238ebeb7c7f
Richard Backhouse <richard.backhouse@gmail.com>
2012-02-04 17:55:16 +0000

    Added sign in and out

--- main.py
+++ main.py
@@ -9,58 +9,62 @@ from google.appengine.api import users
 from django.utils import simplejson
 
 class Score(db.Model):
-    user = db.UserProperty()
-    score = db.IntegerProperty()
-    width = db.IntegerProperty()
-    height = db.IntegerProperty()
-    board = db.StringProperty()
+	user = db.UserProperty()
+	score = db.IntegerProperty()
+	width = db.IntegerProperty()
+	height = db.IntegerProperty()
+	board = db.StringProperty()
 
 class IndexHandler(webapp.RequestHandler):
-    def get(self):
-        tmpl = os.path.join(os.path.dirname(__file__), 'index.html')
-        context = {}
-            # context = {'sign_in_url':users.create_login_url('/'),
-            #             'signed_in' : False,}
-        self.response.out.write(render(tmpl, context))
+	def get(self):
+		tmpl = os.path.join(os.path.dirname(__file__), 'index.html')
+		user = users.get_current_user()
+		if user:
+			context = {'user_message':("Signed in as %s (<a href=\"%s\">sign out</a>)" %
+                        (user.nickname(), users.create_logout_url("/")))}
+		else:
+			context = {'user_message':("<a href=\"%s\">Sign in</a>" % users.create_login_url('/'))}
+			
+		self.response.out.write(render(tmpl, context))
 
 class SubmitGameHandler(webapp.RequestHandler):
-    def get(self):
-        score = Score()
-        score.score = int(self.request.get("score"))
-        score.width = int(self.request.get("width"))
-        score.height = int(self.request.get("height"))
-        score.board = self.request.get("board")
-        score.user = users.get_current_user()
+	def get(self):
+		score = Score()
+		score.score = int(self.request.get("score"))
+		score.width = int(self.request.get("width"))
+		score.height = int(self.request.get("height"))
+		score.board = self.request.get("board")
+		score.user = users.get_current_user()
 
-        score.put()
+		score.put()
 
 class HighScoresHandler(webapp.RequestHandler):
-    def get(self):
-        query = Score.gql("ORDER BY score DESC LIMIT 10")
-        scores = []
-        for score in query:
-            scoreObj = {'score' : score.score,
-                          'width' : score.width, 
-                          'height' : score.height, 
-                          'board' : score.board,
-                          'user' : "Anonymous" }
+	def get(self):
+		query = Score.gql("ORDER BY score DESC LIMIT 10")
+		scores = []
+		for score in query:
+			scoreObj = {'score' : score.score,
+						  'width' : score.width, 
+						  'height' : score.height, 
+						  'board' : score.board,
+						  'user' : "Anonymous" }
 
-            if score.user != None:
-                scoreObj.user = score.user.nickname()
-            scores.append(scoreObj)
-        self.response.out.write(simplejson.dumps(scores))
+			if score.user != None:
+				scoreObj['user'] = score.user.nickname()
+			scores.append(scoreObj)
+		self.response.out.write(simplejson.dumps(scores))
 
 class DeleteScoresHandler(webapp.RequestHandler):
-    def get(self):
-        db.delete(Score.all())
+	def get(self):
+		db.delete(Score.all())
 
 def main():
-    application = webapp.WSGIApplication([('/', IndexHandler),
-                                          ('/submitGame', SubmitGameHandler),
-                                          ('/getHighScores', HighScoresHandler),
-                                          ('/deleteScores', DeleteScoresHandler)],
-                                         debug = True)
-    util.run_wsgi_app(application)
+	application = webapp.WSGIApplication([('/', IndexHandler),
+										  ('/submitGame', SubmitGameHandler),
+										  ('/getHighScores', HighScoresHandler),
+										  ('/deleteScores', DeleteScoresHandler)],
+										 debug = True)
+	util.run_wsgi_app(application)
 
 if __name__ == '__main__':
-    main()
+	main()
